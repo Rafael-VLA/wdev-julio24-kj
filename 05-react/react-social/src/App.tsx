3945d2ef94cdb6ea516e77b5fcb84473990be4bb
Rafael-VLA <Rafaelsequeira@vlacademy.com>
2025-03-21 01:59:15 -0600

    Updated react project

--- 05-react/react-social/src/App.tsx
+++ 05-react/react-social/src/App.tsx
@@ -1,11 +1,27 @@
-import { Counter } from './components/Counter';
+
+// import { Counter } from './components/Counter';
+import { CounterEffect } from './components/CounterEffect';
 
 function App() {
   return (
     <div className="container mx-auto">
-      <h1>Hello world</h1>
+      <h1 className="count-color">Hello world</h1>
 
-      <Counter />
+        {/* <Counter initialCount={0} decrementBy={2} title="dsdsds">
+          {
+            (currentCount) => {
+              return (
+                <h1 className="mb-2 text-blue-400">Conter 1 - { currentCount }</h1>
+              )
+            }
+          }
+        </Counter> */}
+
+      {/* <Counter initialCount={0} decrementBy={2}>
+        <h1 className="mb-2 text-blue-400">Conter 1</h1>
+      </Counter> */}
+
+      <CounterEffect />
     </div>
   )
 }
--- 05-react/react-social/src/components/Counter.tsx
+++ 05-react/react-social/src/components/Counter.tsx
@@ -1,11 +1,75 @@
-// import { Fragment } from "react"
+import { useState, FC, Fragment, ReactNode, CSSProperties } from 'react'
+// import './counter-styles.css' // => styles con clases (globales) 
+// import styles from './counter.module.css'; // => styles con módulos
 
-export const Counter = () => {
+// Tarea: Definir la props decrementBy que sea opcional y que tenga un valor por defecto de 1
+interface Props {
+    initialCount?: number;
+    title?: string;
+    incrementBy?: number;
+    decrementBy?: number;
+    children: (value: number) => ReactNode
+    // title: ReactNode
+}
+
+// const containerStyles = (count: number) => {
+
+//     return { 
+//         color: count % 2 === 0 ? "red" : "blue" 
+//     }
+// }
+
+
+// export const Counter = ({ initialCount, incrementBy = 1 }: Props) => {
+export const Counter: FC<Props> = ({ initialCount = 1, incrementBy = 1, decrementBy = 1, children }) => { // FC = Funcional Component
+
+    console.log("Mi initial", initialCount)
+
+    const [count, setCount] = useState<number>(initialCount); // [ state, setState ]
+
+    console.log("Me renderizo");
+
+    const containerStyles: CSSProperties = { 
+        // color: count % 2 === 0 ? "red" : "blue" 
+        color: "#888",
+    }
+
+    const increment = (): void => {
+        setCount(count + incrementBy)
+    };
+
+    //Tarea: Crear el método decrement y usarlo en el evento del botón
+    const decrement = (): void => {
+
+
+        setCount((currentCount) => {
+
+            if (currentCount > 0) { // Mientras el contador sea mayor que 0
+                const result = currentCount - decrementBy; // 1 < 0
+                return result < 0 ? 0 : result; // Se decrementa
+            }
+
+            console.log("Devuelvo el mismo valor", currentCount)
+
+            return currentCount; // Si no, se mantiene le mismo valor
+        })
+
+
+    };
+
+    // {{  }} => { }
     return (
-        <div className="flex items-center gap-x-2">
-            <button className="bg-blue-500 text-white px-3 py-2 rounded-lg cursor-pointer hover:bg-blue-400 transition-colors">Decrement</button>
-            <span>0</span>
-            <button className="bg-blue-500 text-white px-3 py-2 rounded-lg cursor-pointer hover:bg-blue-400 transition-colors">Increment</button>
-        </div>
+        <Fragment>
+            { children(count) }
+
+            <div className="flex items-center gap-x-2 count-color">
+                <button onClick={decrement} className="btn-primary">-</button>
+                {/* <span className={ styles.countLine }>{count}</span> */}
+                <span style={containerStyles}>{count}</span>
+                <button onClick={increment} className="btn-primary">+</button>
+            </div>
+
+            {/* <hr className="mt-4" /> */}
+        </Fragment>
     )
 }
